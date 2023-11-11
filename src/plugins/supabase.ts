da42782844ec68310b6d0cad0eedb6a893a9b051
Dawid <dawidkwasowski@gmail.com>
2023-11-11 20:39:04 +0100

    feat(Supabase): Add the supabase configuration

---
+++ src/plugins/supabase.ts
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+export const supabase = createClient(
+   'https://qecibrgkamoswnaafsvb.supabase.co',
+   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlY2licmdrYW1vc3duYWFmc3ZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyOTI3NjgsImV4cCI6MjAxNDg2ODc2OH0.XTI_u-Cbs8FdKwcAEQvLd9bRFm1NZQgm0HyXhiOSsQE'
+)
