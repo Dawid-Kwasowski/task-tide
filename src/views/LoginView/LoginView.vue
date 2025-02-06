bc5e87664f462a9eca7f36ebe06947bb7ade9a09
Dawid <dawidkwasowski@gmail.com>
2025-02-06 20:04:05 +0100

    fix(RWD): problems with mobile view have been fixed

--- src/views/LoginView/LoginView.vue
+++ src/views/LoginView/LoginView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen">
+  <div>
     <div class="ma-5 d-flex justify-end">
       <v-btn
         @click="router.push({ name: 'Account' })"
@@ -7,10 +7,10 @@
         icon="mdi-account"
       ></v-btn>
     </div>
-    <div class="d-flex align-center h-100">
+    <div>
       <v-container>
         <v-window v-model="step">
-          <v-window-item value="0" class="pa-6">
+          <v-window-item value="0">
             <profiles-component @create-profile="step = 1" />
           </v-window-item>
           <v-window-item value="1" class="pa-6">
@@ -27,7 +27,21 @@ import { ref } from "vue";
 import ProfilesComponent from "@/views/LoginView/components/ProfilesComponent/ProfilesComponent.vue";
 import NewProfileComponent from "@/views/LoginView/components/NewProfileComponent/NewProfileComponent.vue";
 import { useRouter } from "vue-router";
+import { supabase } from "@/plugins/supabase";
+import { useUserStore } from "@/stores/UserStore/UserStore";
 
+const userStore = useUserStore();
 const router = useRouter();
 const step = ref<number>(0);
+
+supabase
+  .channel("profiles-all-channel")
+  .on(
+    "postgres_changes",
+    { event: "*", schema: "public", table: "profiles" },
+    async () => {
+      await userStore.fetchProfiles();
+    },
+  )
+  .subscribe();
 </script>
